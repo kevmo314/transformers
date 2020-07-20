9a41bdfc5a59b464bfe8ab0f9584e62a60333dbf
Kevin Wang <kevmo314@gmail.com>
2020-07-19 23:39:31 -0400

    Analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,9 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: { trackingId: "UA-19960251-5" },
+    },
   ],
 };
